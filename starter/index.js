56b7eeaedb12f3f0d9fc20c995b2582a4bd8e51a
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-01-13 13:52:10 +0000

    Completed the inquirer  questions array and init function to initialize the program

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the Table of Contents?',
+        name: 'tableofcontent',
+    },
+    {
+        type: 'input',
+        message: 'What is the Installation process?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What are the usage details of your project?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a licence for your project?',
+        name: 'licence',
+        choices: ["license1", "license2", "license3"],
+    },
+    {
+        type: 'input',
+        message: 'Who are the contributors?',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'What are the test details of your project?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Please provide your email address?',
+        name: 'email',
+    },
 
 ];
 
@@ -15,6 +61,13 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            response.length === questions.length? console.log('Success') : console.log('You forgot to complete some sections!');
+            const myAnswer = generateMarkdown(response);
+            writeToFile('sample.md', myAnswer);
+        })
 
 }
 
